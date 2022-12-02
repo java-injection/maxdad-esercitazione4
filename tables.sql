38c1fdf9ed6f101b30448d6319525798a38e6f12
Luca <luca.coraci@gmail.com>
2022-12-02 17:20:45 +0100

    added procedure to -if exists- a foreign key

--- tables.sql
+++ tables.sql
@@ -1,9 +1,32 @@
 -- create table di tutte le tabelle
 USE esercitazione4;
 
+
+DROP PROCEDURE IF EXISTS PROC_DROP_FOREIGN_KEY;
+DELIMITER $$
+CREATE PROCEDURE PROC_DROP_FOREIGN_KEY(IN tableName VARCHAR(64), IN constraintName VARCHAR(64))
+BEGIN
+    IF EXISTS(
+            SELECT * FROM information_schema.table_constraints
+            WHERE
+                    table_schema    = DATABASE()     AND
+                    table_name      = tableName      AND
+                    constraint_name = constraintName AND
+                    constraint_type = 'FOREIGN KEY')
+    THEN
+        SET @query = CONCAT('ALTER TABLE ', tableName, ' DROP FOREIGN KEY ', constraintName, ';');
+        PREPARE stmt FROM @query;
+        EXECUTE stmt;
+        DEALLOCATE PREPARE stmt;
+    END IF;
+END$$
+DELIMITER ;
+
+
 -- create table sensori
 -- drop foreign kei if exists fk_sensori
-ALTER TABLE data_sensori DROP FOREIGN KEY fk_id_sensor;
+-- ALTER TABLE data_sensori DROP FOREIGN KEY fk_id_sensor ;
+CALL PROC_DROP_FOREIGN_KEY('data_sensori', 'fk_id_sensor');
 
 DROP TABLE IF EXISTS sensori;
 

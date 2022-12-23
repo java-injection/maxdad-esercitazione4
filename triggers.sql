47d6056d17805d2514bd274e137ced70c3d14241
Max <massimo.m.morgantini@gmail.com>
2022-12-23 18:29:59 +0100

    WIP anomaly triggers

--- triggers.sql
+++ triggers.sql
@@ -1,9 +1,9 @@
 -- triggers
-DROP TRIGGER IF EXISTS Anomalia2_Before_Insert;
+DROP TRIGGER IF EXISTS Anomalia2_After_Insert;
 
 DELIMITER &&
-CREATE TRIGGER Anomalia2_Before_Insert
-    BEFORE INSERT ON data_sensori
+CREATE TRIGGER Anomalia2_After_Insert
+    AFTER INSERT ON data_sensori
     FOR EACH ROW
     BEGIN
         IF(NEW.data is null)
@@ -19,27 +19,30 @@ CREATE TRIGGER Anomalia2_Before_Insert
 DELIMITER ;
 
 
-DROP TRIGGER IF EXISTS Anomalia1_Before_Insert;
+DROP TRIGGER IF EXISTS Anomalia1_After_Insert;
 
 DELIMITER $$
-CREATE TRIGGER Anomalia1_Before_Insert
-    BEFORE INSERT ON data_sensori
+CREATE TRIGGER Anomalia1_After_Insert
+    AFTER INSERT ON data_sensori
     FOR EACH ROW
-    BEGIN
-        IF(1 <= (SELECT count(*)
-                FROM data_sensori
-                WHERE timestamp = NEW.timestamp AND id_sensor = NEW.id_sensor))
-        THEN
-                BEGIN
-                    INSERT INTO anomalies VALUES (new.timestamp, new.id_sensor, 'ANOMALY 1');
-                    DELETE FROM data_sensori WHERE timestamp = OLD.timestamp AND id_sensor = OLD.id_sensor;
-                    SET @exception = CONCAT('SONO STATI TROVATI 2 O PIU VALORI CON LO STESSO TIMESTAMP');
-                    SIGNAL SQLSTATE '02000'
-                    SET MESSAGE_TEXT = @exception;
-                END;
-        END IF;
-    END $$
+BEGIN
+    IF(1 <= (SELECT count(*)
+             FROM data_sensori
+             WHERE timestamp = NEW.timestamp AND id_sensor = NEW.id_sensor))
+    THEN
+        BEGIN
+            INSERT INTO anomalies VALUES (NEW.timestamp, NEW.id_sensor, 'ANOMALY 1');
+            DELETE FROM data_sensori WHERE timestamp = NEW.timestamp AND id_sensor = NEW.id_sensor;
+            SET @exception = CONCAT('SONO STATI TROVATI 2 O PIU VALORI CON LO STESSO TIMESTAMP');
+            SIGNAL SQLSTATE '02000'
+                SET MESSAGE_TEXT = @exception;
+        END;
+    END IF;
+END $$
 DELIMITER ;
+
+
+
 
 
 
